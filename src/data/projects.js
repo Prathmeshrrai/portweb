6c16288354dd5249363840b95adafccb1997ea5d
raiprathmesh <raiprathmesh71@gmail.com>
2024-09-02 12:34:41 +0530

    url specification

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,24 +1,6 @@
-import stonks from "../assets/images/stonks.png";
 import sendfrom from "../assets/images/sendform.jpg";
-import semicolon from "../assets/images/semicolon.png";
-import scroll from "../assets/images/scroll.jpg";
-import getv from "../assets/images/get-v.jpg";
 
 export const projects = [
-  /*{
-    title: "Get Vulnerable",
-    image: getv,
-    link: "https://get-vulnerable.teshank.dev",
-    description:
-      "Game which encourages the players to open up by asking questions",
-  },*/
-  /*{
-    title: "Semicolon",
-    image: semicolon,
-    link: "https://github.com/teshank2137/semicolon.md",
-    description:
-      "Mental health support by predicting and offering activities for mood upliftment.",
-  },*/
   {
     title: "Send form to me",
     image: sendfrom,
@@ -26,57 +8,11 @@ export const projects = [
     description:
       "Backend-as-a-service for creating forms with unique URLs and email notifications.",
   },
-  /*{
-    title: "Fan Merch",
-    image:
-      "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/fanmerch.png",
-    github: "https://github.com/teshank2137/fanMerch",
-    link: "https://fan-merch.vercel.app/",
-    description:
-      "A conceptual website for social media influence's to sell their merch.",
-  },
-  /*{
-    title: "Find Techie",
-    description:
-      "A Twitter bot that scans Twitter for job/intern posting and retweets top tweets daily.",
-    image:
-      "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/Techie.png",
-    link: "https://twitter.com/FindTechie",
-  },*/
-  /*{
-    title: "React Infinite scroll",
-    description: "Infinite scrolling using custom hook and ObservableAPI",
-    image: scroll,
-    link: "https://react-infinite-scroll-nu.vercel.app/",
-    github: "https://github.com/teshank2137/react-infinite-scroll",
-  },*/
-  /*{
-    title: "Smart Attendance App",
-    description:
-      "A mobile app in which the user can track and mark attendance using face verification.",
-    image:
-      "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/smartattendance.png",
-    github: "https://github.com/teshank2137/smartAtendence",
-  },
-  {
-    title: "Stock Market Prediction API",
-    description:
-      "An API Which predicts the next day's closing price of a stock.",
-    image: stonks,
-    github: "https://github.com/teshank2137/stockprediction",
-  },
-  {
-    title: "Algorithm Visualizer",
-    description: "Web App to visualize the searching and sorting algorithms",
-    image:
-      "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/sort.JPG",
-    link: "https://teshank2137.github.io/#/",
-  },*/
   {
     title: "Portfolio",
     description: "Built using HTML, CSS, Javascript, ReactJS and lots of ❤️ love.",
     image:
       "https://png.pngtree.com/png-vector/20210206/ourmid/pngtree-p-alphabet-vector-golden-design-png-image_2884855.png",
     link: "https://prathmeshrrai.github.io/portweb/",
-  },
+  }
 ];
